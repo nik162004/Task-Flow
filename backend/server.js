470d3325ff6c33b460376d6efccf519e870775e1
Nik <nik40662@gmail.com>
2026-08-01 15:59:03 +0530

    Initial backend setup with MongoDB

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res)=>{
+    res.send("TaskFlow Backend is Running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
